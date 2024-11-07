501c4f222a7900506143b2337bcd7cf8f7ca2b30
andreinapazz <108377491+andreinapazz@users.noreply.github.com>
2024-11-07 03:20:06 +0000

    first-commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["our", "her", "the", "them"];
+let adj = ["amazing", "disgusting", "incredible", "unbelievable"];
+let place = ["lab", "kitchen", "mall", "island"];
+let extension = [".com", ".net", ".ve", ".gob"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < place.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        console.log(pronoun[i] + adj[j] + place[k] + extension[l]);
+      }
+    }
+  }
+}
